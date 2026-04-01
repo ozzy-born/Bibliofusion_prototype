f4cd6c75eb8144017eb98427ee86d874def966f6
bodina <leo.razafindralambo@gmail.com>
2026-04-01 17:43:57 +0200

    test 1

--- Bibliofusion prototype/Program.cs
+++ Bibliofusion prototype/Program.cs
@@ -16,7 +16,7 @@ namespace Bibliofusion_prototype
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main_Form());
+            Application.Run(new MainForm());
         }
     }
 }
